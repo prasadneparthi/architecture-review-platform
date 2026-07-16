f638d313a11ab67ce5ac73eda493be4f79178311
prasad N <prasadneparthi2003@gmail.com>
2026-07-16 19:44:37 +0530

    Configure Production Settings

--- config/settings.py
+++ config/settings.py
@@ -31,9 +31,12 @@ SECRET_KEY=os.getenv("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG","False")=="True"
 ENABLE_LLM=False
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = os.getenv(
+    "ALLOWED_HOSTS",
+    "127.0.0.1,localhost"
+).split(",")
 
 
 # Application definition
