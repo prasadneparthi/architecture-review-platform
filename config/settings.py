80d5f8b7362acc8b3678456b1bc6be9c7402ab24
prasad N <prasadneparthi2003@gmail.com>
2026-07-16 20:19:05 +0530

    llm enabled

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY=os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG","False")=="True"
-ENABLE_LLM=False
+ENABLE_LLM=True
 ALLOWED_HOSTS = os.getenv(
     "ALLOWED_HOSTS",
     "127.0.0.1,localhost"
